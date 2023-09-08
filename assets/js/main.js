ec89dc5d531de247bdb3cd5dc93a798fd21638c4
Susheell <kmr89susheel@gmail.com>
2023-09-08 09:53:52 +0530

    Initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,132 @@
+$(function () {
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > (
+            $(window).height()) / 100) {
+            $('header').addClass("header-fixed");
+        } else {
+            $('header').removeClass("header-fixed");
+        }
+    });
+    // Open Model
+    $('.model-open').on('click', function (e) {
+        e.preventDefault();
+        var model = $(this).attr('data-model');
+        $(model).addClass('is-open');
+    });
+    // Close Model
+    $('.close_model').on('click', function () {
+        $(this).parents('.model').removeClass('is-open');
+        $('.humburgerbg').removeClass('is-open');
+        $('.video_full iframe').attr('src', '');
+    });
+
+    $('.overlay').on('click', function () {
+        $(this).parent('.model').removeClass('is-open');
+    });
+
+    $('.model-video').on('click', function () {
+        var src = $(this).attr('data-video');
+        $('.video_full iframe').attr('src', src);
+    });
+    $('.form-control').each(function () {
+        if ($(this).val() != '') {
+            $(this).parent('.form-group').addClass('valid');
+        } else {
+            $(this).parent('.form-group').removeClass('valid');
+        }
+    });
+    $('.form-control').on('change', function () {
+        if ($(this).val() != '') {
+            $(this).parent('.form-group').addClass('valid');
+        } else {
+            $(this).parent('.form-group').removeClass('valid');
+        }
+    });
+
+    //
+
+    $('.down-arrow img').click(function () {
+        $('html, body').animate({
+            scrollTop: $(".home-secA").offset().top
+        }, 500);
+    });
+
+    $('.showcase-slider').owlCarousel({
+        loop: true,
+        margin: 0,
+        dots: false,
+        navText: ["<img src='assets/icons/prev.png'>", "<img src='assets/icons/next.png'>"],
+        smartSpeed: 1200,
+        autoplay: false,
+        responsive: {
+            0: {
+                items: 1.3,
+                margin: 20,
+                nav: false,
+            },
+            768: {
+                items: 2.3,
+                margin: 20,
+                nav: true,
+            },
+            992: {
+                items: 2.5,
+                margin: 10,
+                nav: true,
+            }
+        }
+    });
+
+    $('.service-slider').owlCarousel({
+        loop: true,
+        margin: 0,
+        dots: false,
+        navText: ["<img src='assets/icons/prev.png'>", "<img src='assets/icons/next.png'>"],
+        smartSpeed: 1200,
+        autoplay: false,
+        responsive: {
+            0: {
+                items: 1.3,
+                margin: 20,
+                nav: false,
+            },
+            768: {
+                items: 2.3,
+                margin: 10,
+                nav: true,
+            },
+            992: {
+                items: 3,
+                margin: 15,
+                nav: true,
+            }
+        }
+    });
+
+    $('.client-slider').owlCarousel({
+        loop: true,
+        margin: 0,
+        dots: false,
+        navText: ["<img src='assets/icons/prev.png'>", "<img src='assets/icons/next.png'>"],
+        smartSpeed: 1200,
+        autoplay: false,
+        responsive: {
+            0: {
+                items: 1.3,
+                margin: 20,
+                nav: false,
+            },
+            768: {
+                items: 2.3,
+                margin: 10,
+                nav: true,
+            },
+            992: {
+                items: 1.5,
+                margin: 0,
+                nav: true,
+            }
+        }
+    });
+
+});
